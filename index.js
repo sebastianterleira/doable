abd10e763927216f24736867c371410f6be02e70
sebastianterleira <seb.terleira1204@gmail.com>
2022-11-24 11:32:47 -0500

    add: apiFetch & session service

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import { login, logout } from "./src/services/sessions-service.js";
 
-const crendentials = {
+const credentials = {
 	email: "seb.terleira1204@mail.com",
 	password: "123456",
 };
 
 async function test() {
 	try {
-		const user = await login(crendentials);
+		const user = await login(credentials);
 		console.log(user);
 
-		const data = await logout(); 
-		console.log(data);
+		// const data = await logout(); 
+		// console.log(data);
 	} catch (error) {
 		console.log(error);
 	}
 }
-
 test();
--- src/services/sessions-service.js
+++ src/services/sessions-service.js
@@ -1,48 +1,18 @@
 // Crear el Login y el Logout
-import { BASE_URI, tokenKey } from "../config.js";
+import apiFetch from "./api-fetch.js";
+import { tokenKey } from "../config.js";
 
+// Crear el Login y el Logout
 export async function login(credentials = { email, password }) {
-    const response = await fetch(`${BASE_URI}/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(credentials),
-    });
+	const { token, ...user } = await apiFetch("login", { body: credentials });
+	sessionStorage.setItem(tokenKey, token);
+	console.log(token);
 
-    if (!response.ok) {
-        const data = await response.json();
-        throw new Error(data.errors);
-    }
-
-    const data = await response.json();
-    sessionStorage.setItem(tokenKey, data.token);
-
-    return data;
+	return user;
 }
 
 export async function logout() {
-	const token = sessionStorage.getItem(tokenKey);
-
-	const response = await fetch(`${BASE_URI}/logout`, {
-			method: "DELETE",
-			headers: {
-				Authorization: `Token token=${token}`,
-				"Content-Type": "application/json",
-			},
-	});
-
-	let data;
-	try {
-		data = await response.json();
-	} catch (error) {
-		data = response.statusText;
-	}
-
-	if (!response.ok) {
-		throw new Error(data.errors);
-	}
-
+	const data = await apiFetch("logout", { method: "DELETE" });
 	sessionStorage.removeItem(tokenKey);
 
 	return data;
